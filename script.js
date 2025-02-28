9d79c0c65c292cc32329a53d74a7bd63bde59517
chrisdemee <526cdemetri@frhsd.com>
2025-02-28 09:09:52 -0500

    all java works

--- script.js
+++ script.js
@@ -2,13 +2,63 @@
 let tasks = []
 
 //Add on ON CLICK event listener to the "add task button" that calls a function
-document.getElementById('addTaskBtn').addEventListener('click', function (){
+document.getElementById('addTaskBtn').addEventListener('click', function () {
     //Get the value of the input box and store it in a variable called task input
-    let taskInput = document.getElementById(taskInput).value
+    let taskInput = document.getElementById("taskInput").value
     //check if task input has something in it
-    if(taskInput){
-        
+    if (taskInput) {
+        //add new tasks to the array
+        tasks.push(taskInput)
+
+        document.getElementById('taskInput').value = ''
+
+        displayTasks()
     }
 
-    
+    console.log(tasks)
+
+})
+
+// function that will display lists 
+function displayTasks() {
+
+    let taskList = document.getElementById('taskList')
+
+    taskList.innerHTML = ''
+
+    //loop through every task in the array for each item in the array it will create a new list item
+    tasks.forEach((task, index) => {
+        //make new <li> element for each task
+        let li = document.createElement('li')
+
+        //Add css classes for styling 
+        li.classList.add(
+            'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center'
+        )
+
+        li.innerHTML = `${task} <button class='btn btn-dark btn-sm' onclick='removeTask(${index})'> √ </button>`
+
+        taskList.appendChild(li)
+    }
+
+    )
+
+}
+
+
+//remove task from list when check button is clicked 
+function removeTask(index){
+    tasks.splice(index, 1)
+
+    displayTasks()
+}
+
+//event listener for clear all tasks button 
+document.getElementById('clearTaskBtn').addEventListener('click', function () {
+
+
+//empty tasks array to remove all tasks
+tasks = []
+//call the function to update task list display
+displayTasks()
 })
